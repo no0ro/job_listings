0a56bab3bd4f7cb47febf582f37feec957d2e7f7
Noelle Rolwes <nrolwes@gmail.com>
2020-01-17 16:02:19 -0600

    added cli and listing files to lib filder

---
+++ lib/job_listings/listing.rb
@@ -0,0 +1,17 @@
+# my environment
+# all files point here
+
+require "pry" # testing
+require "rest-client" # request api
+require "json" # parse api
+
+
+
+
+
+
+
+
+require "./job_listings/cli"
+require "./job_listings/listing"
+require "./job_listings/version"
